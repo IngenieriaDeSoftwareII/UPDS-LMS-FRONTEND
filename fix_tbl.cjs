a47a4702d5cd79f279fb95e402a13b054a55a8ba
Francisco Paolo Gomez Pocoaca <141799500+PanchoGmZ@users.noreply.github.com>
2026-04-05 01:42:52 -0400

    Delete fix_tbl.cjs

--- fix_tbl.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let path = 'src/pages/admin/CategoriesPage.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-// Also update the empty span and the edit modal logic
-content = content.replace(/colSpan=\{3\}/, 'colSpan={4}');
-content = content.replace(/No se encontraron categor\u00C3\u00ADas./g, 'No se encontraron categorías.');
-content = content.replace(/TableHead>Descripci.*?n<\/TableHead>/, 'TableHead>Descripción</TableHead>');
-fs.writeFileSync(path, content, 'utf8');
